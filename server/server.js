97a0fe304e0a41108cf9c06787a455a7aa399f1a
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-16 16:22:28 -0500

    Set up route and listen

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({msg: 'Welcome to the app'})  
+})
+
+app.listen(4000, () => {
+    console.log('listening on port 4000');
+});
