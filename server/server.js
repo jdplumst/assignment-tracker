02e1e0cbac38135c075601b774179f0d170811bc
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-16 18:09:21 -0500

    Add assignments.js

--- server/server.js
+++ server/server.js
@@ -4,9 +4,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
 app.get('/', (req, res) => {
     res.json({msg: 'Welcome to the app!'})  
-})
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`);
