28bd24a22bfe222a5e5f269cbe10c8bc61bf23a1
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-16 16:39:43 -0500

    Add nodemon script

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to the app'})  
+    res.json({msg: 'Welcome to the app!'})  
 })
 
 app.listen(process.env.PORT, () => {
