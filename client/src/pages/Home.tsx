f8cc5b83790ec1218635158f4138fb96585a3a6e
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-18 16:24:20 -0500

    Fetch data from backend

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from 'react';
+
+type Assignment = {
+    _id: String,
+    title: String,
+    course: String,
+    dueDate: Date,
+    createdAt: Date,
+    updatedAt: Date,
+    __v: number
+}
+
 const Home = () => {
+    const [assignments, setAssignments] = useState<Assignment[]>([]);
+
+    useEffect(() => {
+        const fetchAssignments = async () => {
+            const response = await fetch('/api/assignments');
+            const data = await response.json();
+            if (response.ok) {
+                setAssignments(data);
+            }
+        }
+        fetchAssignments();
+    }, []);
+
     return (
         <div>
-            <h2>Home</h2>
+            {assignments.map((assignment) => (
+                <p key={0}>${assignment.title}</p>
+            ))}
         </div>
     );
 };
