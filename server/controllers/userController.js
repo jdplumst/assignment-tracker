bb0cc99da4277cd3390be36d3fb1d5c447296b54
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-21 21:44:05 -0500

    Add functionality to generate JWT for signup and login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,20 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1h" });
+};
 
 // Login user
 const loginUser = async (req, res) => {
-  res.json({ msg: "login user" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 // Signup user
@@ -10,7 +22,8 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -9,6 +9,22 @@ const userSchema = new Schema({
   password: { type: String, required: true }
 });
 
+// Static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect email");
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect password");
+  }
+  return user;
+};
+
 // Static signup method
 userSchema.statics.signup = async function (email, password) {
   if (!email || !password) {
