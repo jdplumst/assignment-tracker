a22de71b8a1cdb384a8f72a47b7dc2994b9af947
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-21 19:06:08 -0500

    Add functionality for signup and password hashing

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
+
+// Static signup method
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await this.create({ email, password: hash });
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
