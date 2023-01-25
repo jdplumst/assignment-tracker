d78c4c7a37154241e1cf7e349ee5354fd3c0bc65
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-24 23:35:17 -0500

    Redirect users based on their authentication status

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useUserContext } from "./hooks/useUserContext";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,13 +7,24 @@ import Signup from "./pages/Signup";
 import "./styles/styles.css";
 
 const App = () => {
+  const { userState } = useUserContext();
+
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={userState.user ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!userState.user ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!userState.user ? <Login /> : <Navigate to="/" />}
+        />
       </Routes>
     </BrowserRouter>
   );
